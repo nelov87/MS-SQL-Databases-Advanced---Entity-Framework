88da8d3ea26e459e79b1f4b60e756a41d7aff3ac
Nelov87 <nelov87@gmail.com>
2019-02-24 21:55:44 +0200

    update for MiniORM

---
+++ 02. ORM Fundamentals/MiniORM.App/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+using System;
+using System.Linq;
+
+namespace MiniORM.App
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var connectionSrting = @"Server=DESKTOP-533LOVH\SQLEXPRESS;Database=MiniORM;Integrated Security=true";
+
+            var context = new SoftUniDbContext(connectionSrting);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Goshko",
+                LastName = "Goshkov",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            var employee = context.Employees.Last();
+            employee.FirstName = "Modifaied";
+
+            context.SaveChanges();
+        }
+    }
+}
